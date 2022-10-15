df563f866172bbe50dde634979f31444c7dafec0
Lance <lance@Lances-MBP.attlocal.net>
2022-10-15 14:20:38 -0500

    Added project files

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x5f7B7558B59071f8622dFd541D2a46a0aE907936";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
